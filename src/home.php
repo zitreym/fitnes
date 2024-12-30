a62eb548c6da20ac380cd85ae661f85af99e856a
BaranovD <baranovd@ro.ru>
2024-12-30 17:03:30 +0300

    4

--- src/home.php
+++ src/home.php
@@ -1,7 +1,7 @@
 <body>
 <div class="sign_page">
     <p class="sign_txt">Добро пожаловать на мою страницу, запишитесь онлайн:</p>
-    <a href="#" class="sign_btn">Записаться онлайн</a>
+    <a href="/form" class="sign_btn">Записаться онлайн</a>
     <p class="sign_txt">Связаться со мной:</p>
 </div>
 
