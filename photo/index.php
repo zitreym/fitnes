1fd740d33aceefb75526dba9fe1f9900a78bef91
BaranovD <baranovd@ro.ru>
2024-12-30 16:51:44 +0300

    1

--- photo/index.php
+++ photo/index.php
@@ -1,12 +1,5 @@
 <?php
-require("src/header.php");
-?>
-<body>
-<div class="sign_page">
-    <p class="sign_txt">тут фото</p>
-</div>
-
-</body>
-<?php
-require("src/footer.php");
+require("./src/header.php");
+require("./src/photo.php");
+require("./src/footer.php");
 ?>
