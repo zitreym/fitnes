c379b64fa8265bc1ec9c00acef69e095c4ee0474
BaranovD <baranovd@ro.ru>
2024-12-31 00:36:55 +0300

    5

--- src/home.php
+++ src/home.php
@@ -3,9 +3,8 @@
     <p class="sign_txt">Добро пожаловать на мою страницу, запишитесь онлайн:</p>
     <a href="/form" class="sign_btn">Записаться онлайн</a>
     <p class="sign_txt">Связаться со мной:</p>
-    <a href="#" class="link_btn tel_icon">Позвонить</a>
-    <a href="#" class="link_btn tg_icon">Telegram</a>
-    <a href="#" class="link_btn wa_icon">WhatsApp</a>
+    <a href="tel:+79209440332" class="link_btn tel_icon">Позвонить</a>
+    <a href="https://t.me/tatinabaranova" target="_blank" class="link_btn tg_icon">Telegram</a>
+    <a href="https://wa.me/79209440332" target="_blank" class="link_btn wa_icon">WhatsApp</a>
 </div>
-
 </body>
