d68c08655e3c34499239e62445345fa02270e58d
BaranovD <baranovd@ro.ru>
2024-12-30 16:53:43 +0300

    1

--- photo/index.php
+++ photo/index.php
@@ -1,5 +1,5 @@
 <?php
-require("../src/header.php");
-require("../src/photo.php");
-require("../src/footer.php");
+require("/src/header.php");
+require("/src/photo.php");
+require("/src/footer.php");
 ?>
