72cce67f425e31965e72bec761279b63a88b3a0c
BaranovD <baranovd@ro.ru>
2024-12-31 15:48:13 +0300

    8

--- src/info.php
+++ src/info.php
@@ -4,7 +4,7 @@
     <div class="info_maps_box">
         <p class="info_bold_txt">Физкультурно-спортивный комплекс им. Ю.Н. Каперского</p>
         <a href="https://yandex.ru/maps/-/CHQTmUI4" target="_blank" class="info_address_btn">Курлово, ул. Школьная д. 20</a>
-        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad1763f1663198954ea70f332394e29f3e5fee6c095254ab82c5fba2c2a8b5205&amp;source=constructor" width="500" height="250" frameborder="0"></iframe>
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad1763f1663198954ea70f332394e29f3e5fee6c095254ab82c5fba2c2a8b5205&amp;source=constructor" height="250" frameborder="0"></iframe>
     </div>
 </div>
 </body>
