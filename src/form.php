af5286c9308394ad5efe915cf396c27e6ba1e2fd
BaranovD <baranovd@ro.ru>
2025-01-01 22:05:52 +0300

    9

--- src/form.php
+++ src/form.php
@@ -1,5 +1,11 @@
 <body>
 <div class="sign_page">
     <p class="regular_txt">тут будет форма для записи</p>
+<?php
+require $_SERVER['DOCUMENT_ROOT']."/sql.php";
+$result = $mysqli->query("SELECT * FROM fit");
+$result = $result->fetch_all();
+?>
+<p><? var_dump($result) ?></p>
 </div>
 </body>
