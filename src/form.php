371c458e1c7ca819765e0494a0f22c4f2b51da48
BaranovD <baranovd@ro.ru>
2025-01-01 23:05:01 +0300

    9

--- src/form.php
+++ src/form.php
@@ -3,8 +3,8 @@
     <p class="regular_txt">тут будет форма для записи</p>
 <?php
 require $_SERVER['DOCUMENT_ROOT']."/sql.php";
-$result = $mysqli->query("SELECT * FROM fit");
-
+$result = $mysqli->query("SELECT * FROM fit date >= NOW()");
+$result = $result->fetch_all();
 ?>
 <p><? var_dump($result) ?></p>
 </div>
