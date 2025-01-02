0c3bc45218476b87f69c8b51bc232d6341efd2f5
BaranovD <baranovd@ro.ru>
2025-01-02 15:53:20 +0300

    10

--- src/form.php
+++ src/form.php
@@ -7,16 +7,9 @@
             $name_user = $_POST['name_user'];
             $phone_user = $_POST['phone_user'];
             $fitchose_user = $_POST['fitchose_user'];
-            $name_user = htmlspecialchars($name_user);
-            $phone_user = htmlspecialchars($phone_user);
-            $name_user = urldecode($name_user);
-            $phone_user = urldecode($phone_user);
-            $name_user = trim($name_user);
-            $phone_user = trim($phone_user);
             if ($fitchofitchose_user > 1) {
-$data = array( 'name' => $name_user, 'phone' => $phone_user, 'fitchose' => $fitchose_user);
-$query = $mysqli->prepare("INSERT INTO $db_table (name, phone, fitchose) values (:name, :phone, :fitchose)");
-$query->execute($data);
+$query = "INSERT INTO $db_table (name, phone, fitchose) values ($name_user, $phone_user, $fitchose_user)";
+$resultinsert->execute($query);
             }
              ?>" method="post">
         <input type="text" class="form_input" placeholder="ВАШЕ ИМЯ*" name="name_user" required>
