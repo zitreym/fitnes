e7f0f19d9b76de8166b1b2e66e66aed4c16e238f
BaranovD <baranovd@ro.ru>
2025-01-02 16:11:14 +0300

    10

--- src/form.php
+++ src/form.php
@@ -6,10 +6,8 @@
             $name_user = $_POST['name_user'];
             $phone_user = $_POST['phone_user'];
             $fitchose_user = $_POST['fitchose_user'];
-            if ($fitchofitchose_user != null) {
 $query = "INSERT INTO form (name, phone, fitchose) values ('fff', '445345', '2')";
 $resultinsert->execute($query);
-            }
              ?>" method="post">
         <input type="text" class="form_input" placeholder="ВАШЕ ИМЯ*" name="name_user" required>
         <input type="text" class="form_input" placeholder="НОМЕР ТЕЛЕФОНА" name="phone_user" required>
