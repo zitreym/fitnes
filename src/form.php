51ec1b9753187ffc35b693cc5b138b3c1baa36a3
BaranovD <baranovd@ro.ru>
2025-01-04 00:07:03 +0300

    10

--- src/form.php
+++ src/form.php
@@ -2,11 +2,25 @@
 <div class="sign_page">
     <form action="<?php
     require $_SERVER['DOCUMENT_ROOT']."/sql.php";
+    require $_SERVER['DOCUMENT_ROOT']."/botsettings.php";
             $name_user = $_POST['name_user'];
             $phone_user = $_POST['phone_user'];
             $fitchose_user = $_POST['fitchose_user'];
             if ($fitchose_user > 0) {
 $query = $mysqli->query("INSERT INTO form (name, phone, fitchose) values ('$name_user', '$phone_user', $fitchose_user)");
+$message_for_tg = "Новая запись на тренировку номер $fitchose_user от $name_user с телефоном $phone_user";
+$getQuery = array(
+    "chat_id" 	=> $admin,
+    "text"  	=> $message_for_tg,
+    "parse_mode" => "html"
+);
+$ch = curl_init("https://api.telegram.org/bot". $token ."/sendMessage?" . http_build_query($getQuery));
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HEADER, false);
+$resultQuery = curl_exec($ch);
+curl_close($ch);
+
+
             }
              ?>" method="post" class='form_sign'>
         <p class="form_txt_info">Запись на тренировку:</p>
