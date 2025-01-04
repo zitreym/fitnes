385f794906139b6e32267db1f5341f275a76c88e
BaranovD <baranovd@ro.ru>
2025-01-04 17:50:49 +0300

    11

--- src/info.php
+++ src/info.php
@@ -2,11 +2,6 @@
 <div class="regular_page">
     <p class="regular_txt bold_txt">Информация:</p>
     <div class="info_box">
-        <p class="info_bold_txt info_bold_txt_maps">Физкультурно-спортивный комплекс им. Ю.Н. Каперского</p>
-        <a href="https://yandex.ru/maps/-/CHQTmUI4" target="_blank" class="info_address_btn">Курлово, ул. Школьная д. 20</a>
-        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad1763f1663198954ea70f332394e29f3e5fee6c095254ab82c5fba2c2a8b5205&amp;source=constructor" height="250" frameborder="0"></iframe>
-    </div>
-    <div class="info_box">
         <p class="info_bold_txt">Мои контакты:</p>
         <div class="info_tel_box">
             <p class="info_tel_txt">Тел:</p>
@@ -18,6 +13,11 @@
     <div class="info_box">
         <p class="info_bold_txt">Я в социальных сетях:</p>
         <a href="https://vk.com/id107021619" target="_blank" class="info_address_btn_vk">Вконтакте</a>
+    </div>
+    <div class="info_box">
+        <p class="info_bold_txt info_bold_txt_maps">Физкультурно-спортивный комплекс им. Ю.Н. Каперского</p>
+        <a href="https://yandex.ru/maps/-/CHQTmUI4" target="_blank" class="info_address_btn">Курлово, ул. Школьная д. 20</a>
+        <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ad1763f1663198954ea70f332394e29f3e5fee6c095254ab82c5fba2c2a8b5205&amp;source=constructor" height="250" frameborder="0"></iframe>
     </div>
 </div>
 </body>
