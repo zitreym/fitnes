a0b423d65a420e57277bd5762eb14abc6a9a06e3
BaranovD <baranovd@ro.ru>
2025-01-06 19:27:54 +0300

    14

--- bot.php
+++ bot.php
@@ -3,7 +3,6 @@ require("botsettings.php");
 require("sql.php");
 $data = file_get_contents('php://input');
 $data = json_decode($data, true);
-$otvet = var_export($data, true);
 $userid = $data['message']['from']['id'];
 $username = $data['message']['from']['first_name'];
 $lastname = $data['message']['from']['last_name'];
@@ -11,10 +10,7 @@ $nameid = $data['message']['from']['username'];
 $text = $data['message']['text'];
 $textArray = explode(", ", $text);
 
-if (empty($data['message']['chat']['id'])) {
-	exit();
-}
-
+if (empty($data['callback_query']['data'])) {
 switch ($textArray[0]) {
     case 'добавить':
         $query = $mysqli->query("INSERT INTO fit (name, description, date, price) values ('$textArray[1]', '$textArray[2]', '$textArray[3]', '$textArray[4]')");
@@ -29,9 +25,9 @@ switch ($textArray[0]) {
         curl_setopt($ch, CURLOPT_HEADER, false);
         $resultQuery = curl_exec($ch);
         curl_close($ch);
+        $query_tg = $mysqli->query("INSERT INTO tglog (log) values ('$resultQuery')");
         break;
     case '/start':
-        $query = $mysqli->query("INSERT INTO fit (name, description, date, price) values ('$textArray[1]', '$textArray[2]', '$textArray[3]', '$textArray[4]')");
         $message_for_tg = "Это фитнес бот, чтобы добавить тренировку, напишите: добавить, название тренировки, описание тренировки, 2025-01-05 17:00, 250";
         $getQuery = array(
             "chat_id" 	=> $userid,
@@ -58,5 +54,53 @@ switch ($textArray[0]) {
         curl_setopt($ch, CURLOPT_HEADER, false);
         $resultQuery = curl_exec($ch);
         curl_close($ch);
+        $query_tg = $mysqli->query("INSERT INTO tglog (log) values ('$resultQuery')");
         break;
+}
+}
+else {
+switch ($data['callback_query']['data']) {
+    case '/change':
+        $message_for_tg = "Вы выбрали изменить тренировку";
+        $getQuery = array(
+            "chat_id" 	=> $data['callback_query']['from']['id'],
+            "text"  	=> $message_for_tg,
+            "parse_mode" => "html"
+        );
+        $ch = curl_init("https://api.telegram.org/bot". $token ."/sendMessage?" . http_build_query($getQuery));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $resultQuery = curl_exec($ch);
+        curl_close($ch);
+        $query_tg = $mysqli->query("INSERT INTO tglog (log) values ('$resultQuery')");
+        break;
+    case '/delete':
+        $message_for_tg = "Вы выбрали удалить тренировку";
+        $getQuery = array(
+            "chat_id" 	=> $data['callback_query']['from']['id'],
+            "text"  	=> $message_for_tg,
+            "parse_mode" => "html"
+        );
+        $ch = curl_init("https://api.telegram.org/bot". $token ."/sendMessage?" . http_build_query($getQuery));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $resultQuery = curl_exec($ch);
+        curl_close($ch);
+        $query_tg = $mysqli->query("INSERT INTO tglog (log) values ('$resultQuery')");
+        break;
+    default:
+        $message_for_tg = "Я не знаю такую команду ответа";
+        $getQuery = array(
+            "chat_id" 	=> $data['callback_query']['from']['id'],
+            "text"  	=> $message_for_tg,
+            "parse_mode" => "html"
+        );
+        $ch = curl_init("https://api.telegram.org/bot". $token ."/sendMessage?" . http_build_query($getQuery));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $resultQuery = curl_exec($ch);
+        curl_close($ch);
+        $query_tg = $mysqli->query("INSERT INTO tglog (log) values ('$resultQuery')");
+        break;
+}
 }
