d8ae0481101ee5da4b25867c2e3db99a5d80e90b
BaranovD <baranovd@ro.ru>
2025-01-26 00:35:12 +0300

    20

--- src/photo.php
+++ src/photo.php
@@ -1,5 +1,23 @@
 <body>
 <div class="regular_page">
-    <p class="regular_txt">тут фото</p>
+    <div class="photo_wrapper">
+        <?
+        require $_SERVER['DOCUMENT_ROOT']."/sql.php";
+        $all_photo = $mysqli->query("SELECT src FROM photo");
+        $all_photo = $all_photo->fetch_all();
+        foreach ($all_photo as $photo) {
+            ?><div class="photo_box" style="background-image: url(../static/photo/<?echo $photo[0];?>);"></div><?
+        }
+        $count_photo=count($all_photo);
+        $ostatok = ($count_photo % 3);
+        if ($ostatok > 0) {
+            $ostatok = 3 - $ostatok;
+            while ($ostatok>0) {
+                ?><div class="photo_box_empty"></div><?
+                $ostatok = $ostatok-1;
+            }
+        }
+        ?>
+    </div>
 </div>
 </body>
