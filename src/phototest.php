b38009e553a7b0ad8f7fc75c516aaa8881f66695
BaranovD <baranovd@ro.ru>
2025-01-26 00:32:32 +0300

    20

--- src/phototest.php
+++ src/phototest.php
@@ -8,6 +8,15 @@
         foreach ($all_photo as $photo) {
             ?><div class="photo_box" style="background-image: url(../static/photo/<?echo $photo[0];?>);"></div><?
         }
+        $count_photo=count($all_photo);
+        $ostatok = ($count_photo % 3);
+        if ($ostatok > 0) {
+            $ostatok = 3 - $ostatok;
+            while ($ostatok>0) {
+                ?><div class="photo_box_empty"></div><?
+                $ostatok = $ostatok-1;
+            }
+        }
         ?>
     </div>
 </div>
