84d9c81aeddbcd4f3f41da6e3b74913051642fb2
BaranovD <baranovd@ro.ru>
2025-01-26 18:19:26 +0300

    20

--- src/photo.php
+++ src/photo.php
@@ -6,7 +6,9 @@
         $all_photo = $mysqli->query("SELECT src FROM photo");
         $all_photo = $all_photo->fetch_all();
         foreach ($all_photo as $photo) {
-            ?><div class="photo_box" style="background-image: url(../static/photo/<?echo $photo[0];?>);"></div><?
+            $photoname = explode(".", $photo[0]);
+            ?><div class="photo_box" onclick="toggleVisibility('<?echo $photoname[0];?>')" style="background-image: url(../static/photo/<?echo $photo[0];?>);"></div>
+              <div class="photo_hidden" id="<?echo $photoname[0];?>" onclick="toggleVisibility('<?echo $photoname[0];?>')" style="background-image: url(../static/photo/<?echo $photo[0];?>);"></div><?
         }
         $count_photo=count($all_photo);
         $ostatok = ($count_photo % 3);
@@ -21,3 +23,13 @@
     </div>
 </div>
 </body>
+<script>
+function toggleVisibility(info) {
+  let element = document.getElementById(info);
+  if (element.style.display === 'none') {
+    element.style.display = 'block';
+  } else {
+    element.style.display = 'none';
+  }
+}
+</script> 
